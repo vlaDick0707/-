34b66cfdc24af9a5e8f1d9d4bbbe0a993c67b5e2
Владислав Рыжанков <vladik_ryzhankov007@outlook.com>
2024-12-18 22:25:41 +0300

    added files

---
+++ Abonent.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneService
+{
+    public class Abonent
+    {
+        public string Name { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public List<Service> Services { get; private set; }
+        public Bill Account { get; set; }
+        public bool IsBlocked { get; set; }
+        public Abonent(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Services = new List<Service>();
+            IsBlocked = false;
+        }
+
+        public void SelectService(Service service)
+        {
+            Services.Add(service);
+            Console.WriteLine($"Абонент {Name} выбрал услугу {service.Name}.");
+        }
+
+
+        public void UnselectService(Service service)
+        {
+            Services.Remove(service);
+            Console.WriteLine($"Абонент {Name} отказался от услуги {service.Name}.");
+        }
+
+
+        public void PayBill(Bill bill)
+        {
+            if (bill.IsPaid)
+            {
+                Console.WriteLine("Счет уже оплачен.");
+            }
+            else
+            {
+                bill.Pay();
+                Console.WriteLine($"{Name} оплатил счет.");
+            }
+        }
+    }
+}
---
+++ PhoneService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneService
+{
+    public static class PhoneService
+    {
+        public static void ActivateService(Abonent abonent, Service service)
+        {
+            abonent.Services.Add(service);
+            Console.WriteLine($"Услуга {service.Name} активирована для абонента {abonent.Name}.");
+        }
+
+        public static void DeactivateService(Abonent abonent, Service service)
+        {
+            abonent.Services.Remove(service);
+            Console.WriteLine($"Услуга {service.Name} деактивирована для абонента {abonent.Name}.");
+        }
+    }
+}
